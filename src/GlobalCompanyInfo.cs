bbb64b48df683eb779c94c825228a720de34360d
Eric Falsken <eric@everylittlething.net>
2016-07-06 09:02:00 -0700

    Moved attributes into the AssemblyInfo.cs. Removed GlobalCompanyInfo.cs

--- src/GlobalCompanyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Demand Driven Technologies, LLC.")]
-[assembly: AssemblyCopyright("© Demand Driven Technologies, LLC. 2004-2015")]
-[assembly: AssemblyTrademark("DBR+ is a trademark of Demand Driven Technologies, LLC. \r\n\"DDT, Demand Driven Technologies\", Replenishment+ and R+ are all registered trademarks of Demand Driven Technologies, LLC.")]
