e0553282bacaf5f246220d0ecb2c8177db162bde
Ty Norton <github@bsd.bz>
2016-07-15 10:58:20 -0700

    Pending Orders: Transition => Commit

--- src/DDTech.Localization/PendingOrders.Designer.cs
+++ src/DDTech.Localization/PendingOrders.Designer.cs
@@ -214,7 +214,7 @@ namespace DDTech.Localization {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Download All Pending Orders (No Transition).
+        ///   Looks up a localized string similar to Download All Pending Orders (No Commit).
         /// </summary>
         public static string DownloadAllPendingOrders {
             get {
@@ -660,24 +660,6 @@ namespace DDTech.Localization {
         public static string TotalOrders {
             get {
                 return ResourceManager.GetString("TotalOrders", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Transition All Pending Orders to &quot;Exported&quot;.
-        /// </summary>
-        public static string TransitionAllPendingOrders {
-            get {
-                return ResourceManager.GetString("TransitionAllPendingOrders", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Transition Selected Pending Orders to &quot;Exported&quot;.
-        /// </summary>
-        public static string TransitionSelectedPendingOrders {
-            get {
-                return ResourceManager.GetString("TransitionSelectedPendingOrders", resourceCulture);
             }
         }
         
