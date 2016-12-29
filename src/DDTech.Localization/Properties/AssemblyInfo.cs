c2419d701f86fc57b67c8590d6376be150719f72
Ty Norton <tynorton@gmail.com>
2016-12-29 12:37:09 -0800

    Clean Up Assembly Version Upgrade NuGets

--- src/DDTech.Localization/Properties/AssemblyInfo.cs
+++ src/DDTech.Localization/Properties/AssemblyInfo.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyCompany("Demand Driven Technologies, LLC.")]
-[assembly: AssemblyCopyright("© Demand Driven Technologies, LLC. 2004-2015")]
+[assembly: AssemblyCopyright("© Demand Driven Technologies, LLC. 2004-2017")]
 [assembly: AssemblyTrademark("DBR+ is a trademark of Demand Driven Technologies, LLC. \r\n\"DDT, Demand Driven Technologies\", Replenishment+ and R+ are all registered trademarks of Demand Driven Technologies, LLC.")]
 [assembly: AssemblyDescription("Demand Driven String Resources (DDSR)")]
